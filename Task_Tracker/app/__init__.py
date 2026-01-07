d19f380b6db9aabacb3a45f42ff422f89b69cd17
DAKURISAIPRANAYREDDY <dakuripranayreddy17@gmail.com>
2026-01-07 22:03:44 +0530

    Modify create_app to support testing configuration

--- Task_Tracker/app/__init__.py
+++ Task_Tracker/app/__init__.py
@@ -4,23 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__, instance_relative_config=True)
     app.config["SECRET_KEY"] = "your_secret_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+
+    if testing:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    else:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
-    # Import routes and register blueprints
     from .routes import main
     app.register_blueprint(main)
 
     from .api import api_bp
     app.register_blueprint(api_bp, url_prefix="/api")
 
-    # Create database if it doesn't exist
     with app.app_context():
         db.create_all()
 
     return app
+
