0d41a3c25a75f543bf50a00935e4a5abca82bd66
DAKURISAIPRANAYREDDY <dakuripranayreddy17@gmail.com>
2026-01-07 21:15:22 +0530

    Update models.py

--- Task_Tracker/app/models.py
+++ Task_Tracker/app/models.py
@@ -1,12 +1,9 @@
 from . import db
-from datetime import datetime
+
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(200))
+    description = db.Column(db.String(255))
     done = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f'<Task {self.title}>'
