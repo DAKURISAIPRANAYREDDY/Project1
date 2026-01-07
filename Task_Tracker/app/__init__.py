5de8f912d6e5aa3f901d901f810f3339cb7f8bb5
DAKURISAIPRANAYREDDY <dakuripranayreddy17@gmail.com>
2026-01-07 21:11:50 +0530

    Fix string quotes in app initialization

--- Task_Tracker/app/__init__.py
+++ Task_Tracker/app/__init__.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SECRET_KEY"] = "your_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
@@ -16,7 +17,7 @@ def create_app():
     app.register_blueprint(main)
 
     from .api import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     # Create database if it doesn't exist
     with app.app_context():
